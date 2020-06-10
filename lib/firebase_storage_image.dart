7cfc546e5d364b434a8a69f8b063f2b0c806c07c
kaumudpa <kaumud@cueclad.com>
2020-06-10 21:37:24 +0530

    Changes to work with latest depedencies

--- lib/firebase_storage_image.dart
+++ lib/firebase_storage_image.dart
@@ -3,14 +3,16 @@ library firebase_storage_image;
 import 'dart:ui' show Codec, hashValues;
 import 'package:firebase_core/firebase_core.dart' show FirebaseApp;
 import 'package:firebase_storage/firebase_storage.dart' show FirebaseStorage;
-import 'package:flutter/foundation.dart' show SynchronousFuture;
+import 'package:flutter/foundation.dart'
+    show SynchronousFuture, DiagnosticsProperty;
 import 'package:flutter/painting.dart'
     show
-        ImageConfiguration,
-        ImageProvider,
-        ImageStreamCompleter,
-        MultiFrameImageStreamCompleter,
-        PaintingBinding;
+    ImageConfiguration,
+    ImageProvider,
+    DecoderCallback,
+    ImageStreamCompleter,
+    MultiFrameImageStreamCompleter,
+    PaintingBinding;
 
 /// Fetches the given URL from Firebase Cloud Storage, associating it with the
 /// given scale.
@@ -26,11 +28,11 @@ class FirebaseStorageImage extends ImageProvider<FirebaseStorageImage> {
   /// [firebaseApp] is optional. By default, the initial firebase app will be
   /// used.
   const FirebaseStorageImage(
-    this.location, {
-    this.scale = 1.0,
-    this.maxSizeBytes = 1000 * 1000,
-    FirebaseApp firebaseApp,
-  })  : assert(location != null),
+      this.location, {
+        this.scale = 1.0,
+        this.maxSizeBytes = 1000 * 1000,
+        FirebaseApp firebaseApp,
+      })  : assert(location != null),
         assert(scale != null),
         assert(maxSizeBytes != null),
         _firebaseApp = firebaseApp;
@@ -51,18 +53,15 @@ class FirebaseStorageImage extends ImageProvider<FirebaseStorageImage> {
       SynchronousFuture<FirebaseStorageImage>(this);
 
   @override
-  ImageStreamCompleter load(FirebaseStorageImage key) {
-    ///// Here Chetan Commited some line code
-    return MultiFrameImageStreamCompleter(
-        codec: _fetch(key),
-        scale: key.scale,
-//        informationCollector: (information) {
-//          information
-//            ..writeln('Image provider: $this')
-//            ..write('Image key: $key');
-//        }
-        );
-  }
+  ImageStreamCompleter load(FirebaseStorageImage key, DecoderCallback decode) =>
+      MultiFrameImageStreamCompleter(
+          codec: _fetch(key),
+          scale: key.scale,
+          informationCollector: () sync* {
+            yield DiagnosticsProperty<FirebaseStorageImage>(
+                'Image provider', this);
+            yield DiagnosticsProperty<FirebaseStorageImage>('Image key', key);
+          });
 
   @override
   bool operator ==(Object other) {
@@ -86,11 +85,12 @@ class FirebaseStorageImage extends ImageProvider<FirebaseStorageImage> {
     var pathlist = key.location.split('${uri.scheme}://${uri.authority}');
     // print("pathlist[0] ********  ${pathlist[0]}");
     // print("pathlist[1] ********  ${pathlist[1]}");
+
     final storage = FirebaseStorage(
       app: key._firebaseApp,
       storageBucket: _getBucketUrl(uri),
+//    ).ref().child(uri.path);
     ).ref().child(pathlist[1]);
-
     final bytes = await storage.getData(key.maxSizeBytes);
 
     return await PaintingBinding.instance.instantiateImageCodec(bytes);
